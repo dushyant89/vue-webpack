1e154a5bf201b51d04cf003c697137a1dbe147a9
dushyant <dushyant.sabharwal@adyen.com>
2018-06-07 16:31:24 +0200

    adding a reactive property

--- src/views/main-content.vue
+++ src/views/main-content.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="main-content">
         <h1> This is your first component in Vue </h1>
-        <h3> Powered by webpack! </h3>
+        <h3> {{ webpack }} </h3>
     </div>
 </template>
 
 <script>
     export default {
-        name: "main-content"
+        name: "main-content",
+        data: () => ({
+            // reactive data property of the component.
+            webpack: 'Powered by webpack!',
+        }),
     }
 </script>
 
 <style scoped>
+    /*
+     * Some styles so that our first component
+     * looks somewhat special
+    */
     .main-content {
         margin: 0 auto;
         font-family: sans-serif;
+        background-color: #ccf7e2;
+        padding: 10px;
+        border-radius: 5px;
+        max-width: 500px;
     }
 </style>
 
